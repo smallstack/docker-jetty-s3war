7d6d1df96b2236ddd7c5e9f6b28c162c7f60b3ce
Maximilian Friedmann <max@smallstack.io>
2017-04-13 16:51:55 +0200

    improving startup logging

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,9 +1,11 @@
 set -e
 if [[ $WAR_URL ]]; then
-  echo "Downloading war file..."Ï
+  echo "Downloading war file from $WAR_URL"
   curl -L -o ROOT.war $WAR_URL
+  echo "Copying war file to JETTY_HOME/webapps ($JETTY_HOME/webapps)"
   cp ROOT.war $JETTY_HOME/webapps
   cd $JETTY_HOME
+  echo "Starting jetty with command: java -jar start.jar jetty.home=$JETTY_HOME"
   java -jar start.jar jetty.home=$JETTY_HOME
 else
   echo "=> Please provide a WAR_URL environment variable!"
