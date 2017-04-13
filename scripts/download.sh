de30215b2099f11b5d1a9676e960d842bc784195
Maximilian Friedmann <max@smallstack.io>
2017-04-13 11:28:28 +0200

    removing logging

--- scripts/download.sh
+++ scripts/download.sh
@@ -4,7 +4,7 @@ if [[ $WAR_URL ]]; then
   curl -L -o ROOT.war $WAR_URL
   cp ROOT.war $JETTY_HOME/webapps
   cd $JETTY_HOME
-  java -jar start.jar --module=logging jetty.home=$JETTY_HOME
+  java -jar start.jar jetty.home=$JETTY_HOME
 else
   echo "=> Please provide a WAR_URL environment variable!"
 fi
