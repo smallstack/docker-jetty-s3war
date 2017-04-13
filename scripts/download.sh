7f62533b17025beb7ba241a51a7b02d4c8bb71d4
Maximilian Friedmann <max@smallstack.io>
2017-04-13 18:02:54 +0200

    Update download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,11 +1,12 @@
 set -e
- if [[ $WAR_URL ]]; then
-   echo "Downloading war file..."
-    curl -L -o ROOT.war $WAR_URL
-    cp ROOT.war $JETTY_HOME/webapps
-    cd $JETTY_HOME
-    export JETTY_HOME=$JETTY_HOME
-    java -jar start.jar jetty.home=$JETTY_HOME
-  else
-    echo "=> Please provide a WAR_URL environment variable!"
-  fi
+if [[ $WAR_URL ]]; then
+  echo "Downloading war file from $WAR_URL"
+  curl -L -o ROOT.war $WAR_URL
+  echo "Copying war file to JETTY_HOME/webapps ($JETTY_HOME/webapps)"
+  cp ROOT.war $JETTY_HOME/webapps
+  cd $JETTY_HOME
+  echo "Starting jetty with command: java -jar start.jar jetty.home=$JETTY_HOME"
+  java -jar start.jar jetty.home=$JETTY_HOME
+else
+  echo "=> Please provide a WAR_URL environment variable!"
+fi
