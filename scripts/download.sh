80756fe0642cf5178187099e0907b0b530dc0e20
Maximilian Friedmann <max@smallstack.io>
2017-04-13 17:58:36 +0200

    Update download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -4,7 +4,8 @@ set -e
     curl -L -o ROOT.war $WAR_URL
     cp ROOT.war $JETTY_HOME/webapps
     cd $JETTY_HOME
-    java -jar start.jar --module=logging jetty.home=$JETTY_HOME
+    export JETTY_HOME=$JETTY_HOME
+    java -jar start.jar jetty.home=$JETTY_HOME
   else
     echo "=> Please provide a WAR_URL environment variable!"
   fi
